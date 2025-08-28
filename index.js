d0333e127c1734575dd0d92ce5ce66ce6b4dff3b
Bavneet Singh <bavneet@Bavneets-MacBook-Pro.local>
2025-08-28 23:27:03 +0100

    links to all routes

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/projects', (req, res) =>{
+  res.render('projects');
+});
+
+app.get('/cv', (req, res) => {
+  res.render('cv');
+});
+
+app.get('/blogs', (req, res) =>{
+  res.render('blogs');
+});
+
+app.get('/photography', (req, res) => {
+  res.render('photography');
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
